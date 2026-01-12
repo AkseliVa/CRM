1717e65b6510f5c9eca3cb04f27f7efc730ca117
AkseliVa <vartiainen.akseli736@gmail.com>
2026-01-12 09:53:05 +0200

    added basic routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router"
 import './index.css'
-import App from './App.tsx'
+import Dashboard from "./pages/dashboard/Dashboard.tsx"
+import Companies from "./pages/companies/Companies.tsx"
+import Customers from "./pages/customers/Customers.tsx"
+import Tickets from "./pages/tickets/Tickets.tsx"
+import Users from "./pages/users/Users.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = document.getElementById('root');
+if (!root) throw new Error("No root element found");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="companies" element={<Companies />} />
+      <Route path="customers" element={<Customers />} />
+      <Route path="tickets" element={<Tickets />} />
+      <Route path="users" element={<Users />} />
+    </Routes>
+  </BrowserRouter>
 )
